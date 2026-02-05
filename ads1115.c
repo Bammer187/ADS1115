2e94594c8462bee2eb677636e20572e5ba9773ff
Lukas Bammer <lukas.bammer@bulme.at>
2026-02-05 13:02:37 +0100

    Created write_register()

--- ads1115.c
+++ ads1115.c
@@ -1,5 +1,20 @@
 #include "ads1115.h"
 
+static esp_err_t write_register(ads1115_t *ads, uint8_t reg, uint16_t data)
+{
+	if (ads == NULL || ads->i2c_handle == NULL) {
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    uint8_t buffer[3];
+    buffer[0] = reg;
+    buffer[1] = (uint8_t)(data >> 8);   // MSB
+    buffer[2] = (uint8_t)(data & 0xFF); // LSB
+    
+    return i2c_master_transmit(ads->i2c_handle, buffer, sizeof(buffer), -1);
+}
+
+
 ads1115_t ads1115_init(i2c_master_dev_handle_t handle, uint16_t addr)
 {
 	ads1115_t ads;
